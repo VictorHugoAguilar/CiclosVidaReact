2146271a9c6ad202610af9671dd152ce8f1c6f1e
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-07-19 14:06:53 +0200

    Añadiendo ref para crear una referencia en un input

--- src/components/userForm/UserForms.jsx
+++ src/components/userForm/UserForms.jsx
@@ -5,16 +5,24 @@ class UserForm extends Component {
     constructor(props) {
         super(props);
         this.state = { email: '', genero: '' }
+        this.emailInput = React.createRef();
+    }
+
+    componentDidMount() {
+        // console.log(this.emailInput.current);
+        this.emailInput.current.focus();
     }
 
     handleChange = e => {
         this.setState({
-            [e.target.name]: e.target.value });
+            [e.target.name]: e.target.value
+        });
     }
 
     handleChangeSelect = e => {
         this.setState({
-            [e.target.name]: e.target.selectedOptions[0].value });
+            [e.target.name]: e.target.selectedOptions[0].value
+        });
     }
 
     handleSubmit = e => {
@@ -23,30 +31,26 @@ class UserForm extends Component {
     }
 
     render() {
-        return ( <
-            form className = "userForm"
-            onSubmit = { this.handleSubmit } >
-            <
-            input type = "email"
-            name = 'email'
-            value = { this.state.email }
-            onChange = { this.handleChange }
-            placeholder = "Introduce tu mail" /
-            >
-            <
-            br / >
-            <
-            select name = "genero"
-            value = { this.state.genero }
-            onChange = { this.handleChangeSelect } >
-            <
-            option value = "hombre" > Hombre < /option> <
-            option value = "mujer" > Mujer < /option> <
-            /select> <
-            br / >
-            <
-            button type = "submit" > Registrarse < /button> <
-            /form>
+        return (
+            <form className="userForm"
+                onSubmit={this.handleSubmit} >
+                <input type="email"
+                    name='email'
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    placeholder="Introduce tu mail"
+                    ref={this.emailInput}
+                />
+                <br />
+                <select name="genero"
+                    value={this.state.genero}
+                    onChange={this.handleChangeSelect} >
+                    <option value="hombre" > Hombre </option>
+                    <option value="mujer" > Mujer </option>
+                </select>
+                <br />
+                <button type="submit" > Registrarse </button>
+            </form>
         );
     }
 }
